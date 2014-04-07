f904545aeac186fdcffe53dbbc0a6ab9d28c2087
Ignacio Nieto <nietofarias@gmail.com>
2014-04-07 00:57:21 -0300

    initial commit

---
+++ TiradorDados.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace TiradorDeDados
+{
+    partial class TiradorDadosForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.chkFuerza = new System.Windows.Forms.CheckBox();
+            this.chkAgilidad = new System.Windows.Forms.CheckBox();
+            this.chkInteligencia = new System.Windows.Forms.CheckBox();
+            this.chkCarisma = new System.Windows.Forms.CheckBox();
+            this.chkConstitucion = new System.Windows.Forms.CheckBox();
+            this.txtInterval = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.startBtn = new System.Windows.Forms.Button();
+            this.stopBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(253, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Debe estar situado en la pantalla de creación del PJ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Fuerza";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 56);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Agilidad";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 79);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Inteligencia";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 102);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Carisma";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 125);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(65, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Constitución";
+            // 
+            // chkFuerza
+            // 
+            this.chkFuerza.AutoSize = true;
+            this.chkFuerza.Checked = true;
+            this.chkFuerza.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkFuerza.Location = new System.Drawing.Point(94, 31);
+            this.chkFuerza.Name = "chkFuerza";
+            this.chkFuerza.Size = new System.Drawing.Size(15, 14);
+            this.chkFuerza.TabIndex = 6;
+            this.chkFuerza.UseVisualStyleBackColor = true;
+            // 
+            // chkAgilidad
+            // 
+            this.chkAgilidad.AutoSize = true;
+            this.chkAgilidad.Checked = true;
+            this.chkAgilidad.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkAgilidad.Location = new System.Drawing.Point(94, 56);
+            this.chkAgilidad.Name = "chkAgilidad";
+            this.chkAgilidad.Size = new System.Drawing.Size(15, 14);
+            this.chkAgilidad.TabIndex = 7;
+            this.chkAgilidad.UseVisualStyleBackColor = true;
+            // 
+            // chkInteligencia
+            // 
+            this.chkInteligencia.AutoSize = true;
+            this.chkInteligencia.Checked = true;
+            this.chkInteligencia.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkInteligencia.Location = new System.Drawing.Point(94, 79);
+            this.chkInteligencia.Name = "chkInteligencia";
+            this.chkInteligencia.Size = new System.Drawing.Size(15, 14);
+            this.chkInteligencia.TabIndex = 8;
+            this.chkInteligencia.UseVisualStyleBackColor = true;
+            // 
+            // chkCarisma
+            // 
+            this.chkCarisma.AutoSize = true;
+            this.chkCarisma.Location = new System.Drawing.Point(94, 102);
+            this.chkCarisma.Name = "chkCarisma";
+            this.chkCarisma.Size = new System.Drawing.Size(15, 14);
+            this.chkCarisma.TabIndex = 9;
+            this.chkCarisma.UseVisualStyleBackColor = true;
+            // 
+            // chkConstitucion
+            // 
+            this.chkConstitucion.AutoSize = true;
+            this.chkConstitucion.Checked = true;
+            this.chkConstitucion.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkConstitucion.Location = new System.Drawing.Point(94, 125);
+            this.chkConstitucion.Name = "chkConstitucion";
+            this.chkConstitucion.Size = new System.Drawing.Size(15, 14);
+            this.chkConstitucion.TabIndex = 10;
+            this.chkConstitucion.UseVisualStyleBackColor = true;
+            // 
+            // txtInterval
+            // 
+            this.txtInterval.Location = new System.Drawing.Point(139, 118);
+            this.txtInterval.Name = "txtInterval";
+            this.txtInterval.Size = new System.Drawing.Size(39, 20);
+            this.txtInterval.TabIndex = 11;
+            this.txtInterval.Text = "100";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(136, 103);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(79, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Intervalo Clicks";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(179, 125);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(43, 13);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "(msegs)";
+            // 
+            // startBtn
+            // 
+            this.startBtn.Location = new System.Drawing.Point(139, 33);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(75, 23);
+            this.startBtn.TabIndex = 14;
+            this.startBtn.Text = "Start";
+            this.startBtn.UseVisualStyleBackColor = true;
+            this.startBtn.Click += new System.EventHandler(this.startBtn_Click);
+            // 
+            // stopBtn
+            // 
+            this.stopBtn.Location = new System.Drawing.Point(139, 69);
+            this.stopBtn.Name = "stopBtn";
+            this.stopBtn.Size = new System.Drawing.Size(75, 23);
+            this.stopBtn.TabIndex = 15;
+            this.stopBtn.Text = "Stop";
+            this.stopBtn.UseVisualStyleBackColor = true;
+            this.stopBtn.Click += new System.EventHandler(this.stopBtn_Click);
+            // 
+            // TiradorDadosForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(225, 145);
+            this.Controls.Add(this.stopBtn);
+            this.Controls.Add(this.startBtn);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtInterval);
+            this.Controls.Add(this.chkConstitucion);
+            this.Controls.Add(this.chkCarisma);
+            this.Controls.Add(this.chkInteligencia);
+            this.Controls.Add(this.chkAgilidad);
+            this.Controls.Add(this.chkFuerza);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "TiradorDadosForm";
+            this.Text = "Tirador De Dados 18";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.CheckBox chkFuerza;
+        private System.Windows.Forms.CheckBox chkAgilidad;
+        private System.Windows.Forms.CheckBox chkInteligencia;
+        private System.Windows.Forms.CheckBox chkCarisma;
+        private System.Windows.Forms.CheckBox chkConstitucion;
+        private System.Windows.Forms.TextBox txtInterval;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button startBtn;
+        private System.Windows.Forms.Button stopBtn;
+    }
+}
+
---
+++ TiradorDados.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.Threading;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace TiradorDeDados
+{
+    public partial class TiradorDadosForm : Form
+    {
+        private bool activado = false;
+
+
+        public TiradorDadosForm()
+        {
+            InitializeComponent();
+        }
+
+        private void startBtn_Click(object sender, EventArgs e)
+        {
+            this.activado = true;
+            this.escanear();
+        }
+
+        private void stopBtn_Click(object sender, EventArgs e)
+        {
+            this.activado = false;
+        }
+
+        private void escanear()
+        {
+            bool encontrado = false;
+            while (activado && !encontrado)
+            {
+                //Tirar dados
+                int[] destino = { 680, 100};
+                int[] original = this.obtenerPosicionMouse();   //guardar Posicion original
+                this.clickear(destino);     //clickear en los dados
+                this.mover(original);       //Devolver el mouse a posicion original
+
+                this.Hide();
+                Thread.Sleep(Int32.Parse(this.txtInterval.Text));
+                //Sacar una screenShot
+                Bitmap bt = this.capturar();
+                //Evaluar patron
+                if (this.evaluarAtributos(bt))
+                {
+                    encontrado = true;
+                }
+                this.Show();
+                Thread.Sleep(Int32.Parse(this.txtInterval.Text));
+            }
+        }
+
+        private bool evaluarAtributos(Bitmap bt)
+        {
+            /*Constitución:569,137 - Carisma: 569,114 - Inteligencia: 569,91 - Agilidad: 569,68 - Fuerza: 569,46 */
+            bool encontrado = true;
+            if (chkFuerza.Checked) encontrado &= this.esUnOcho(569, 46, bt);
+            if (chkAgilidad.Checked) encontrado &= this.esUnOcho(569, 68, bt);
+            if (chkInteligencia.Checked) encontrado &= this.esUnOcho(569, 91, bt);
+            if (chkCarisma.Checked) encontrado &= this.esUnOcho(569, 114, bt);
+            if (chkConstitucion.Checked) encontrado &= this.esUnOcho(569, 137, bt);
+            return encontrado;
+        }
+
+        private bool esUnOcho(int x, int y, Bitmap bt)
+        {
+            int[,] blancos = {    //  Matríz de 8col x 10filas
+              {0,0,0,1,1,0,0,0},    //---oo---
+              {0,0,1,1,1,1,0,0},    //--oooo--
+              {0,0,0,0,0,0,0,0},    //--------
+              {0,0,0,0,0,0,0,0},    //--------
+              {0,0,1,1,1,1,0,0},    //--oooo--
+              {0,0,1,1,1,1,0,0},    //--oooo--
+              {0,1,0,0,0,0,0,0},    //-o------
+              {0,1,0,0,0,0,1,0},    //-o----o-
+              {0,0,1,1,1,1,0,0},    //--oooo--
+              {0,0,0,1,1,0,0,0}     //---oo---
+            };
+            for (int fila = 0; fila < 10; fila++)
+                for (int columna = 0; columna < 8; columna++)
+                    if (blancos[fila, columna] == 1 && !this.esBlanco(bt.GetPixel(x + columna, y + fila)))
+                        return false;
+            return true;
+        }
+
+        private bool esBlanco(Color color)
+        {
+            return color.R == 255 && color.G == 255 && color.B == 255;
+        }
+
+        /*RELACIONADO CON LOS SCREENSHOTS*/
+        private Graphics screenShot;
+        private Bitmap capturar()
+        {
+            Bitmap bt = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height, PixelFormat.Format32bppArgb);
+            screenShot = Graphics.FromImage(bt);
+            screenShot.CopyFromScreen(Screen.PrimaryScreen.Bounds.X, Screen.PrimaryScreen.Bounds.Y, 0, 0, Screen.PrimaryScreen.Bounds.Size, CopyPixelOperation.SourceCopy);
+            return bt;
+        }
+
+        
+        /*RELACIONADO CON EL MOUSE*/
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const int MOUSEEVENTF_LEFTUP = 0x04;
+        private const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+        private const int MOUSEEVENTF_RIGHTUP = 0x10;
+
+        [StructLayout(LayoutKind.Sequential)]
+        struct MOUSEINPUT { int dx; int dy; int mouseData; public int dwFlags; int time; IntPtr dwExtraInfo; }
+        struct INPUT { public uint dwType; public MOUSEINPUT mi; }
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern uint SendInput(uint nInputs, INPUT[] pInputs, int cbSize);
+
+        private void clickear(int[] posicion) {
+            Cursor.Position = new Point(posicion[0], posicion[1]);
+            var input = new INPUT() { dwType = 0, mi = new MOUSEINPUT() { dwFlags = MOUSEEVENTF_LEFTDOWN } };
+            SendInput(1, new INPUT[] { input }, Marshal.SizeOf(input));
+            input = new INPUT() { dwType = 0, mi = new MOUSEINPUT() { dwFlags = MOUSEEVENTF_LEFTUP } };
+            SendInput(1, new INPUT[] { input }, Marshal.SizeOf(input));
+        }
+
+        private void mover(int[] posicion) {
+            Cursor.Position = new Point(posicion[0], posicion[1]);
+        }
+
+        private int[] obtenerPosicionMouse()
+        {
+            return new int[] { Cursor.Position.X, Cursor.Position.Y };
+        }
+
+        /*RELACIONADO CON HOOK DEL TECLADO PARA EL ESCAPE
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WM_KEYDOWN = 0x0100;
+        private static LowLevelKeyboardProc _proc = HookCallback;
+        private static IntPtr _hookID = IntPtr.Zero;
+
+        public static void Main()
+        {
+            _hookID = SetHook(_proc);
+            Application.Run();
+            UnhookWindowsHookEx(_hookID);
+        }
+
+        private static IntPtr SetHook(LowLevelKeyboardProc proc)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx(WH_KEYBOARD_LL, proc, GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+        private delegate IntPtr LowLevelKeyboardProc( int nCode, IntPtr wParam, IntPtr lParam);
+
+        private static IntPtr HookCallback( int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+            {
+                int vkCode = Marshal.ReadInt32(lParam);
+                Console.WriteLine((Keys)vkCode);
+            }
+            return CallNextHookEx(_hookID, nCode, wParam, lParam);
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+         */
+    }
+}
